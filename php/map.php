6483dc7816044e920fa8a577a472427a31e3fd19
subxiang <darkart@gmail.com>
2013-11-01 19:19:37 +0800

    added a map

---
+++ php/map.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Food Map</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+	<style>
+		html, body, #map-canvas {
+			height: 100%;
+			margin: 0px;
+			padding: 0px
+		}
+	</style>
+	
+	<script src="https://www.googleapis.com/maps/api/js?sensor=false">
+	</script>
+	
+	<script>
+		function initialize() {
+			var options = {
+				zoom: 8,
+				center: new google.maps.LatLng(-34.397, 150.644),
+				mapTypeId: google.maps.MapTypeId.ROADMAP
+			};
+
+			new google.maps.Map(document.getElementById('map-canvas'), options);
+		}
+
+		google.maps.event.addDomListener(window, 'load', initialize);
+	</script>
+</head>
+<body>
+	<div id='map-canvas'></div>
+</body>
+</html>
