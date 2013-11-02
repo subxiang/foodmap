7eef24c309f834fa6bb106463ebfc01d80efa5b7
subxiang <darkart@gmail.com>
2013-11-02 19:19:40 +0800

    test map event

--- php/map.php
+++ php/map.php
@@ -23,7 +23,24 @@
 				mapTypeId: google.maps.MapTypeId.ROADMAP
 			};
 
-			new google.maps.Map(document.getElementById('map-canvas'), options);
+			var map = new google.maps.Map(document.getElementById('map-canvas'), options);
+
+			var marker = new google.maps.Marker({
+					position: map.getCenter(),
+					map: map,
+					title: 'Click to zoom'
+				});
+
+			google.maps.event.addListener(marker, 'click', function() {
+					map.setZoom(8);
+					map.setCenter(marker.getPosition());
+				});
+
+			google.maps.event.addListener(map, 'center_changed', function() {
+					window.setTimeout(function() {
+							map.panTo(marker.getPosition());
+						}, 3000);
+				});
 		}
 
 		google.maps.event.addDomListener(window, 'load', initialize);
