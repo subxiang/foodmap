6441a4c47d2092d72c6c2a5bec84adb461ce839e
subxiang <darkart@gmail.com>
2013-11-03 16:53:49 +0800

    created a singleton namespace

--- php/script.js
+++ php/script.js
@@ -2,33 +2,59 @@
  * initialize the food map
  */
 
-function createBaseMap() {
-	var options = {
-			zoom: 8,
-			center: new google.maps.LatLng(-34.397, 150.644),
-			mapTypeId: google.maps.MapTypeId.ROADMAP
-		};
-	return new google.maps.Map(document.getElementById('map-canvas'), options);
+var singleton = {};
+
+function createInfo() {
+	var info = new google.maps.InfoWindow({
+		content: 'info',
+		disableAutoPan: true,
+		size: new google.maps.Size(50, 50)
+	});
+	
+	return info;
 }
 
-function createFood(position, url) {
-	var marker = new google.maps.Marker({
-			position: position,
-			icon: url,
-			title: 'Click to zoom'
-		});
+function getInfo() {
+	if (!singleton.info) {
+		singleton.info = createInfo();
+	}
 	
-	return marker;
+	return singleton.info;
+}
+
+function createMap() {
+	var map = new google.maps.Map(document.getElementById('map-canvas'), {
+		zoom: 8,
+		center: new google.maps.LatLng(-34.397, 150.644),
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	});
+	
+	google.maps.event.addListener(map, 'click', function(event) {
+		var food = createFood();
+		food.setPosition(event.latLng);
+		food.setMap(map);
+	});
+	
+	return map;
+}
+
+function createFood(url) {
+	var food = new google.maps.Marker({
+		icon: url,
+		title: 'Click to show information about the food.'
+	});
+	
+	google.maps.event.addListener(food, 'click', function() {
+		var info = getInfo();
+		info.setContent(food.getTitle());
+		info.open(food.getMap(), food);
+	});
+	
+	return food;
 }
 
 function initialize() {
-	var map = createBaseMap();
-	var food = createFood();
-
-	google.maps.event.addListener(map, 'click', function(event) {
-			food.setPosition(event.latLng);
-			food.setMap(map);
-		});
+	createMap();
 }
-		
+
 google.maps.event.addDomListener(window, 'load', initialize);
